18ca85a51489c60d25b85d497d816c02ef4c01cc
DanielPhilosoph <danielph9@gmail.com>
2021-09-22 17:27:32 +0300

    Fixed test bag, done search
    change key name to "tasks" in LocalStorage.
    Added search - working.

--- solution/index.js
+++ solution/index.js
@@ -1,16 +1,26 @@
 // ============================
+// ======== Consts ============
+// ============================
+const KEY_1 = 49;
+const KEY_2 = 50;
+const KEY_3 = 51;
+
+// ============================
 // ====== Main run ============
 // ============================
 document.addEventListener('click', onClickHandler);
-if(localStorage.getItem('task') === null){
-    const task = {
+let searchInput = document.querySelector("#search")
+searchInput.addEventListener('keyup', onKeyUpSearchInput);
+if(localStorage.getItem('tasks') === null){
+    const tasks = {
         "todo": [],
         "in-progress": [],
         "done": []
     }
-    localStorage.setItem('task', JSON.stringify(task));
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
-loadTasksToPage();
+loadTasksToPage("");
+
 
 
 
@@ -40,8 +50,13 @@ function onClickHandler(event){
             case "search":
                 // Do search, will build it later                        
         }
-        loadTasksToPage();   
-    }        
+        loadTasksToPage("");   
+    }    
+}  
+
+function onKeyUpSearchInput(){   
+    let searchQuery = document.getElementById("search").value    
+    loadTasksToPage(searchQuery);
 }
 
 
@@ -51,53 +66,53 @@ function onClickHandler(event){
 *   Section: String, where to add (to-do, prograss, done);
 */
 function AddToSection(taskName, key){
-    const taskManagerDataString = localStorage.getItem('task');
+    const taskManagerDataString = localStorage.getItem('tasks');
     const taskManagerDataJSON = JSON.parse(taskManagerDataString);
     const arrayOfSection = taskManagerDataJSON[key];
     arrayOfSection.unshift(taskName);    
-    localStorage.setItem('task', JSON.stringify(taskManagerDataJSON)); 
-    
-    // Only for me <3 
-    console.log(JSON.parse(localStorage.getItem('task')));  
+    localStorage.setItem('tasks', JSON.stringify(taskManagerDataJSON));         
 }
 
 // ===> gets id of input - return its value (as string) <===
 function getTextFromInputId(id){
     let input = document.querySelector("#" + id);         
     let inputValue = input.value;
+    // Erase input value
     input.value = "";
     return inputValue;
 }
 
-function loadTasksToPage(){
-    const taskManagerDataString = localStorage.getItem('task');
+function loadTasksToPage(query){
+    const taskManagerDataString = localStorage.getItem('tasks');
     const taskManagerDataJSON = JSON.parse(taskManagerDataString);
     let todoDiv = document.querySelector("#div-to-do-tasks");
     todoDiv.innerHTML = "";     
-    let todo_Ul = createTaskUl("todo", "to-do-tasks");
+    let todo_Ul = createTaskUl(query, "todo", ["to-do-tasks"]);
     todoDiv.append(todo_Ul);
 
     let inProgressDiv = document.querySelector("#div-in-progress-tasks");
     inProgressDiv.innerHTML = "";
-    let inProgress_Ul = createTaskUl("in-progress", "in-progress-tasks");
+    let inProgress_Ul = createTaskUl(query, "in-progress", ["in-progress-tasks"]);
     inProgressDiv.append(inProgress_Ul);
 
     let doneDiv = document.querySelector("#div-done-tasks");
     doneDiv.innerHTML = "";
-    let done_Ul = createTaskUl("done", "done-tasks");
+    let done_Ul = createTaskUl(query, "done", ["done-tasks"]);
     doneDiv.append(done_Ul);
     
 }
 
 // ===> Creating UL element from key in "task" <===
-function createTaskUl(key, id){
-    const taskManagerDataString = localStorage.getItem('task');
+function createTaskUl(query, key, classes){
+    const taskManagerDataString = localStorage.getItem('tasks');
     const taskManagerDataJSON = JSON.parse(taskManagerDataString);
     let liArray = [];
     for(let task of taskManagerDataJSON[key]){
-        liArray.push(createElement("li", [task], ["task"]));
+        if(task.includes(query)){
+            liArray.push(createElement("li", [task], ["task"]));
+        }        
     }
-    let ul = createElement("ul", liArray, [], {id: id});  
+    let ul = createElement("ul", liArray, [...classes]);  
     return ul; 
 }
 
