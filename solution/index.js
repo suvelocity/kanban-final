8d0d587d63338f88386ec5819455eae574ac96f7
IdanHerman12 <idanh156@gmail.com>
2021-09-22 18:25:12 +0300

    adding search button

--- solution/index.js
+++ solution/index.js
@@ -1,21 +1,72 @@
-//function that adds a task 
-function addToDoTask(text){
-const li=document.createElement("li")
-li.setAttribute("class","task")
-li.append(text)
-const list=document.querySelector("#main > section > div > ul")
-list.appendChild(li)
-document.getElementById("add-to-do-tasks").value=""
-}
-addToDoTask("boogabooga")
 
-function addTaskEvent(event){
-const text=document.getElementById("add-to-do-tasks").value
+
+
+//function that adds a task to the addTasks section
+function addTask(text){
+const list=document.getElementsByClassName("to-do-tasks")
+const li=createLiElement(text)
+list[0].appendChild(li)
+document.getElementById("add-to-do-task").value=""
+// arrayOfToDoTasks.unshift(text)
+localStorage.setItem("todo",JSON.stringify(text))
+}
+
+//function that adds a task to the addTasks section
+function addInProgress(text){
+    const list=document.getElementsByClassName("in-progress-tasks")
+    const li=createLiElement(text)
+    list[0].appendChild(li)
+    document.getElementById("add-in-progress-task").value=""
+    // const lijs=JSON.stringify({"text":li.value})    
+    // arrayOfToDoTasks.unshift(text)
+    localStorage.setItem("InProgress",text)
+    }
+
+//function the adds a task to done tasks section 
+    function addDoneTasks(text){
+        const list=document.getElementsByClassName("done-tasks")
+        const li=createLiElement(text)
+        list[0].appendChild(li)
+        document.getElementById("add-done-task").value=""
+        localStorage.setItem("doneTasks",text)
+        }
+
+
+
+    function createLiElement(text){
+        const li=document.createElement("li")
+    li.setAttribute("class","task")
+    li.append(text)
+    return li
+    }
+
+function addTaskEvent(){
+const text=document.getElementById("add-to-do-task").value
 if(text==="")alert("cannot add empty task")
 else
-addToDoTask(text)
+addTask(text)
 }
-document.getElementById("submit-add-to-do").addEventListener("click",addTaskEvent)
-// function createElement(tagname){
 
-// }
+
+function inProgressEvent(){
+    const text=document.getElementById("add-in-progress-task").value
+    if(text==="")alert("cannot add empty task")
+    else
+    addInProgress(text)
+    } 
+
+    function doneTasksEvent(){
+        const text=document.getElementById("add-done-task").value
+        if(text==="")alert("cannot add empty task")
+        else
+        addDoneTasks(text)
+        }
+
+
+
+document.getElementById("submit-add-to-do").addEventListener("click",addTaskEvent)
+document.getElementById("submit-add-in-progress").addEventListener("click",inProgressEvent)
+document.getElementById("submit-add-done").addEventListener("click",doneTasksEvent)
+
+
+
