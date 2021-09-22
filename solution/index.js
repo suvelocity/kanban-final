e29cda39743456089ff74d2da9f215c541a11fef
Ido Strauss <strauss02@hotmail.co.il>
2021-09-22 20:07:45 +0300

    add task adding functionality

--- solution/index.js
+++ solution/index.js
@@ -1,18 +1,47 @@
 /**************** Select Elements ****************/
 
 const todoSection = document.querySelector('.todo-section')
+const doneSection = document.querySelector('.done-section')
+const inProgressSection = document.querySelector('.in-progress-section')
+const mainContianer = document.querySelector('.main-container')
+
+const doneTasks = document.querySelector('.done-tasks')
+const todoTasks = document.querySelector('.to-do-tasks')
+const inProgressTasks = document.querySelector('.in-progress-tasks')
 
 const taskBox = document.createElement('div')
 
-function addTaskBox() {}
+/**************** Event Listeners ****************/
+
+mainContianer.addEventListener('click', handleClick)
+
+/**************** Event Handlers ****************/
+
+function handleClick(event) {
+  if (event.target.className.includes('submit-task')) {
+    const list = event.target.parentElement.querySelector('ul')
+    addTaskBox(list)
+  }
+}
+
+/**************** Classes ****************/
 
 class TaskBox extends HTMLElement {
   constructor() {
     super()
-    const container = document.createElement('div')
+  }
+  connectedCallback() {
+    let container = document.createElement('div')
     container.textContent = 'hello'
     this.append(container)
   }
 }
 
 customElements.define('task-box', TaskBox)
+
+/**************** Main Functions ****************/
+
+function addTaskBox(list) {
+  const taskBox = document.createElement('task-box')
+  list.append(taskBox)
+}
