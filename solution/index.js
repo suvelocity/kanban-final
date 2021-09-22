bd60a6bd44c9a3b8a4349272a7a8dcfda0c66f7f
Ziv <zivshaul3@gmail.com>
2021-09-22 21:30:36 +0300

    #4 task complete
    Users can now move tasks between sections using Alt + 1-3

--- solution/index.js
+++ solution/index.js
@@ -1,14 +1,97 @@
+//  Initialazing Data //
 let data
 let localSave = localStorage.getItem("tasks")
 if (localSave){
     data = JSON.parse(localSave)
 }
-else {data = {
+else {
+    data = {
     "todo": [],
     "in-progress": [],
     "done": []
-  }
+    }
 }
+
+//  Data updating  //
+const addNewTaskData = (ul, task) => {
+    
+    if (ul.classList.contains("to-do-tasks")){
+        data.todo.push(task)
+    }
+    if (ul.classList.contains("in-progress-tasks")){
+        data["in-progress"].push(task)
+    }
+    if (ul.classList.contains("done-tasks")){
+        data.done.push(task)
+    }
+    return data
+}
+const removeTaskData = (taskToDelete) => {
+    for (const task of data.todo){
+        if (task == taskToDelete) {
+            data.todo.splice(data.todo.indexOf(task), 1)
+        }
+    }
+    for (const task of data["in-progress"]){
+        if (task == taskToDelete) {
+            data["in-progress"].splice(data["in-progress"].indexOf(task), 1)
+        }
+    }
+    for (const task of data.done){
+        if (task == taskToDelete) {
+            data.done.splice(data.done.indexOf(task), 1)
+        }
+    }
+    return data
+}
+function updateLocalSaveData(){
+    localSave = JSON.stringify(data)
+    localStorage.setItem("tasks", localSave)
+}
+
+
+//  Maniputale tasks order and sections  //
+let taskToMove
+const moveTask = (section) => {
+    // Get the needed data about the task
+    const previusTaskUl = taskToMove.closest("ul")
+    const taskText = taskToMove.innerText
+    // Create the task in it's new section
+    const newTaskElem = createTask(taskText)
+    section.prepend(newTaskElem)
+    console.log(section, taskText)
+    // Removes the task from it's previus section
+    taskToMove.parentElement.removeChild(taskToMove)
+    // Updates local storage
+    removeTaskData(taskText)
+    addNewTaskData(section, taskText)
+    updateLocalSaveData()
+
+}
+function moveBetweenSections({altKey, key}){
+    if (altKey && key == 1){
+        const toDoList = document.querySelector(".to-do-tasks")
+        moveTask(toDoList)
+    }
+    if (altKey && key == 2){
+        const toDoList = document.querySelector(".in-progress-tasks")
+        moveTask(toDoList)
+    }
+    if (altKey && key == 3){
+        const toDoList = document.querySelector(".done-tasks")
+        moveTask(toDoList)
+    }
+}
+function hoverHandler({target}){
+    if (target.classList.contains("task")){
+        taskToMove = target
+        document.addEventListener("keydown", moveBetweenSections)
+        // console.log(target)
+    }
+    target.onmouseout = () => {document.removeEventListener("keypress", moveBetweenSections)}
+}
+
+//  Create and delete elements  //
 const createElement = (tag, content = [], clss = [], attr = {}, eventListener = {}) => {
     const newElem = document.createElement(tag)
     for (let child of content){
@@ -30,46 +113,13 @@ const createElement = (tag, content = [], clss = [], attr = {}, eventListener = 
     }
     return newElem
 }
-const addNewData = (target, task) => {
-    if (target.id === "submit-add-to-do"){
-        data.todo.push(task)
-    }
-    if (target.id === "submit-add-in-progress"){
-        data["in-progress"].push(task)
-    }
-    if (target.id === "submit-add-done"){
-        data.done.push(task)
-    }
-    return data
-}
-const removeData = (taskToDelete) => {
-    for (const elem of data.todo){
-        if (elem == taskToDelete) {
-            data.todo.splice(data.todo.indexOf(elem), 1)
-        }
-    }
-    for (const elem of data["in-progress"]){
-        if (elem == taskToDelete) {
-            data["in-progress"].splice(data["in-progress"].indexOf(elem), 1)
-        }
-    }
-    for (const elem of data.done){
-        if (elem == taskToDelete) {
-            data.done.splice(data.done.indexOf(elem), 1)
-        }
-    }
-}
-const deleteTask = ({target}) => {
+
+function deleteTask({target}) {
     const elementToDelete = target.closest("li")
     elementToDelete.parentElement.removeChild(elementToDelete)
-    removeData(elementToDelete.innerText)
-    localSaveData()
+    removeTaskData(elementToDelete.innerText)
+    updateLocalSaveData()
 }
-function localSaveData(){
-    localSave = JSON.stringify(data)
-    localStorage.setItem("tasks", localSave)
-}
-
 function createTask(task){
     const taskDeleteButton = createElement("button", [""], ["delete-button"], {}, {click: deleteTask})
     const newTaskElem = createElement("li", [task, taskDeleteButton], ["task"])
@@ -87,9 +137,11 @@ function addNewTask({target}){
     const sectionUl = section.querySelector("ul")
     sectionUl.prepend(newTaskElem)
     section.querySelector("input").value = ""
-    addNewData(target, newTask)
-    localSaveData()
+    addNewTaskData(sectionUl, newTask)
+    updateLocalSaveData()
 }
+
+//  Initializing DOM  //
 function generateLists(){
     for (const task of data.todo) {
         const toDoList = document.querySelectorAll(".to-do-tasks")[0]
@@ -108,8 +160,11 @@ function generateLists(){
     }
 }
 
+generateLists()
+
+//  Adding event listeners  //
 const addButtons = document.querySelectorAll(".new-task-button")
 for (const button of addButtons) {
     button.addEventListener("click", addNewTask)
 }
-generateLists()
+document.addEventListener("mouseover", hoverHandler)
