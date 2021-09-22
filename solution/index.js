9b9afd4bd03587143dd1ae289972ed3c399cf964
Ziv <zivshaul3@gmail.com>
2021-09-22 18:40:28 +0300

    #3 task complete
    storing all the tasks in local storage.
    updating the local tasks data when you add or remove tasks

--- solution/index.js
+++ solution/index.js
@@ -1,4 +1,15 @@
-function createElement(tag, content = [], clss = [], attr = {}, eventListener = {}){
+let data
+let localSave = localStorage.getItem("tasks")
+if (localSave){
+    data = JSON.parse(localSave)
+}
+else {data = {
+    "todo": [],
+    "in-progress": [],
+    "done": []
+  }
+}
+const createElement = (tag, content = [], clss = [], attr = {}, eventListener = {}) => {
     const newElem = document.createElement(tag)
     for (let child of content){
         if (typeof child === "string"){
@@ -19,10 +30,52 @@ function createElement(tag, content = [], clss = [], attr = {}, eventListener = 
     }
     return newElem
 }
+const addNewData = (target, task) => {
+    if (target.id === "submit-add-to-do"){
+        data.todo.push(task)
+    }
+    if (target.id === "submit-add-in-progress"){
+        data["in-progress"].push(task)
+    }
+    if (target.id === "submit-add-done"){
+        data.done.push(task)
+    }
+    return data
+}
+const removeData = (taskToDelete) => {
+    for (const elem of data.todo){
+        if (elem == taskToDelete) {
+            data.todo.splice(data.todo.indexOf(elem), 1)
+        }
+    }
+    for (const elem of data["in-progress"]){
+        if (elem == taskToDelete) {
+            data["in-progress"].splice(data["in-progress"].indexOf(elem), 1)
+        }
+    }
+    for (const elem of data.done){
+        if (elem == taskToDelete) {
+            data.done.splice(data.done.indexOf(elem), 1)
+        }
+    }
+}
 const deleteTask = ({target}) => {
     const elementToDelete = target.closest("li")
     elementToDelete.parentElement.removeChild(elementToDelete)
+    removeData(elementToDelete.innerText)
+    localSaveData()
 }
+function localSaveData(){
+    localSave = JSON.stringify(data)
+    localStorage.setItem("tasks", localSave)
+}
+
+function createTask(task){
+    const taskDeleteButton = createElement("button", [""], ["delete-button"], {}, {click: deleteTask})
+    const newTaskElem = createElement("li", [task, taskDeleteButton], ["task"])
+    return newTaskElem
+}
+
 function addNewTask({target}){
     const section = target.closest("section")
     const newTask = section.querySelector("input").value
@@ -30,14 +83,33 @@ function addNewTask({target}){
         alert("OOPS! can't add empty task")
         return
     }
-    const taskDeleteButton = createElement("button", ["❌"], ["delete-button"], {}, {click: deleteTask})
-    const newTaskElem = createElement("li", [newTask, taskDeleteButton], ["task"])
+    const newTaskElem = createTask(newTask)
     const sectionUl = section.querySelector("ul")
     sectionUl.prepend(newTaskElem)
     section.querySelector("input").value = ""
+    addNewData(target, newTask)
+    localSaveData()
+}
+function generateLists(){
+    for (const task of data.todo) {
+        const toDoList = document.querySelectorAll(".to-do-tasks")[0]
+        const newTask = createTask(task)
+        toDoList.appendChild(newTask)
+    }
+    for (const task of data["in-progress"]) {
+        const inProgressList = document.querySelectorAll(".in-progress-tasks")[0]
+        const newTask = createTask(task)
+        inProgressList.appendChild(newTask)
+    }
+    for (const task of data.done) {
+        const doneList = document.querySelectorAll(".done-tasks")[0]
+        const newTask = createTask(task)
+        doneList.appendChild(newTask)
+    }
 }
 
 const addButtons = document.querySelectorAll(".new-task-button")
 for (const button of addButtons) {
     button.addEventListener("click", addNewTask)
 }
+generateLists()
