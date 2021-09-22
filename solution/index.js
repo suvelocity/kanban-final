cce0c067fe41cb870a276c4fd3795f62d29d57e8
galgluska <89578212+galgluska@users.noreply.github.com>
2021-09-22 19:28:57 +0300

    css
    started to edit the website to look better.

---
+++ solution/index.js
@@ -0,0 +1,10 @@
+// const addTask = (taskType ,id) => {
+//     const taskValue = document.getElementById(id).value;
+//     if (!document.getElementById(id).value.replace(/\s/g,'')){
+//         return alert('s')
+//     }
+
+//     document.getElementById(id).value="";
+    
+
+// }
