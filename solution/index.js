7a3f517325027637330d59ae90a8b7c169799b6e
SimpleLogic420 <simplelogic420@gmail.com>
2021-09-22 13:52:31 -0700

    added searchFilter function
    added searchFilter function to filter the li according to text we enter in the search bar

--- solution/index.js
+++ solution/index.js
@@ -28,12 +28,11 @@ const buttonToDo = document.getElementById("submit-add-to-do");
 
 
 
-function addLiGeneric(event){   //A generic function to generate and add tasks (li's).
+function addLiGeneric(event){   //A generic function to generate and add new tasks (li's).
     const target=event.target;
     const section = event.target.closest("section");
-    const ulToAdd =section.querySelector("#ulll");
+    const ulToAdd =section.querySelector(".anyUL");
     const inputToDo = section.querySelector(".add-input").value;
-    alert(inputToDo);
     if(inputToDo.length<1){
         alert("Cant assign an empty task ☹");
     }else{
@@ -71,16 +70,45 @@ function createElement(tagName, children = [], classes = [], attributes = {})
         console.log(child)
         el.append(child)
     }
-
     for (let cls of classes)
     {
         el.classList.add(cls)
     }
-
     for (let attr in attributes)
     {
         el.setAttribute(attr , attributes[attr])
     }
     return el
+}
+const ul1 =document.getElementById("ul1");
+ul1.addEventListener("dblclick", editTask);
+const ul2 =document.getElementById("ul2");
+ul2.addEventListener("dblclick", editTask);
+const ul3 =document.getElementById("ul3");
+ul3.addEventListener("dblclick", editTask);
+ function editTask(event){
+     if(event.target.className==="li-task"){
+        const currentLi = event.target.closest("li");
+      currentLi.setAttribute("contenteditable","true");
+      if(currentLi.blur){
+        currentLi.removeAttribute("contenteditable","true");
+      }
+     }else{
+
+     }
+      
+ }
+//  function that filters the li according to the value of the search input
+ const input = document.getElementById('search');
+input.onkeyup = function () {
+    let filter = input.value.toUpperCase();
+    let lis = document.getElementsByTagName('li');
+    for (var i = 0; i < lis.length; i++) {
+        var name = lis[i].innerHTML;
+        if (name.toUpperCase().indexOf(filter) == 0) 
+            lis[i].style.display = 'list-item';
+        else
+            lis[i].style.display = 'none';
+    }
 }
 
