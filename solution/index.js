1675dc06fef71653afacb30ff8fb4aa04e97382a
ElayGelbart <elaygelbart@gmail.com>
2021-09-22 11:54:25 +0300

    Tasks now show and button works
    we can add task to the to do manager and see our task

---
+++ solution/index.js
@@ -0,0 +1,51 @@
+'use strict'
+let taskObj = {
+    generalTasks: [
+        { nameOfTask: "Bring Down Trash" },
+        { nameOfTask: "Throw Away Dirt" },
+    ],
+    ongoingTasks: [
+        { nameOfTask: "Clean Dishes" },
+    ],
+    finishedTasks: [
+        { nameOfTask: "Clean the Room" },
+    ],
+}
+
+const addTask = (taskType) => {
+    if (taskType === "generalTasks") {
+        const taskValue = document.getElementById("add-to-do-task").value;
+        const newObj = { nameOfTask: taskValue }
+        taskObj[taskType].push(newObj);
+    }
+    else if (taskType === "ongoingTasks") {
+        const taskValue = document.getElementById("add-in-progress-task").value;
+        const newObj = { nameOfTask: taskValue }
+        taskObj[taskType].push(newObj);
+    }
+    else if (taskType === "finishedTasks") {
+        const taskValue = document.getElementById("add-done-task").value;
+        const newObj = { nameOfTask: taskValue }
+        taskObj[taskType].push(newObj);
+    }
+    postTasks();
+}
+
+const postTasks = () => {
+    let generalString = ``, ongoingString = ``, finishedString = ``;
+    for (let key of taskObj.generalTasks) {
+        generalString += `<li class="to-do-tasks">${key.nameOfTask}</li>`
+    }
+    document.getElementById("general-task-table").innerHTML = generalString;
+
+    for (let key of taskObj.ongoingTasks) {
+        ongoingString += `<li class="in-progress-tasks">${key.nameOfTask}</li>`
+    }
+    document.getElementById("ongoing-task-table").innerHTML = ongoingString;
+
+    for (let key of taskObj.finishedTasks) {
+        finishedString += `<li class="done-tasks">${key.nameOfTask}</li>`
+    }
+    document.getElementById("finished-task-table").innerHTML = finishedString;
+}
+postTasks();
