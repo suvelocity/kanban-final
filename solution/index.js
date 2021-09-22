579c95e8a5340da8b1ec147982822fcbd94c1bd1
ArnonAsquira <arnonafking2010@gmail.com>
2021-09-22 14:57:37 +0300

    adding the double click functionality for the list items
    i made two event listeners for the list items for double click and blur

--- solution/index.js
+++ solution/index.js
@@ -1,19 +1,45 @@
 //setting variables for the document elements
 let taskDiv = document.getElementById('tasks-div')
 let taskSectionsArray = Array.from(document.querySelectorAll('.task-section'));
+//an array that holds each section's list items
+ let toDoChidrenArray = [];
+ let inProgressChildrenArray = [];
+ let doneChildrenArray = [];
 
 //adding a list item functionality
-
 function addTask(e){
     let target = e.target;
     if(target.className === 'add-task'){
-        let newTaskInnerItem = createElement('input', children = [], classes = ['taskItemInner'], attributes = {value: `${target.previousElementSibling.value}`, 'disabled' :' disabled'});
-        let newTask = createElement('li',children = [newTaskInnerItem], classes = ['taskItem'], attributes = {});
-        console.log(newTask + 'new task element');
-        target.nextElementSibling.append(newTask);
+        let inputText = target.previousElementSibling.value;
+       if(inputText === ''){
+            alert("You haven't entered any text");
+       }else{
+            let newTaskInnerItem = createElement('input', children = [], classes = ['taskItemInner'], attributes = {value: `${target.previousElementSibling.value}`, 'disabled' :''});
+            let newTask = createElement('li',children = [newTaskInnerItem], classes = ['taskItem'], attributes = {});
+            newTaskInnerItem.addEventListener('blur', saveValueBlur);
+            newTask.addEventListener('dblclick', gainFocus);
+            target.nextElementSibling.insertBefore(newTask, target.nextElementSibling.firstChild);
+            target.previousElementSibling.value = '';
+            
+       }
     }
 }
 taskDiv.addEventListener('click', addTask)
+
+// double click functionality
+
+//gaining focus function
+ function gainFocus(e){
+     let target = e.target;
+     if(target.className === 'taskItemInner'){
+         target.removeAttribute('disabled');
+     }
+ };
+
+ // getting out of focus after blur
+ function saveValueBlur(e){
+    e.target.setAttribute('disabled', '');
+ }
 
 
 
