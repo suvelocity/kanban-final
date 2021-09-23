7f52ccb5a250a5431736faef6573b673e58c6a17
gabbyMill <gabbymillatiner@gmail.com>
2021-09-23 16:47:19 +0300

    Edit function
    Made cards editable with makeeditable attribute.
    On double click it sets the attribute and the 'blur' event listener.
    Afterwards uses splice to replace edited value with old value in parsed version of local storage.

--- solution/index.js
+++ solution/index.js
@@ -120,3 +120,23 @@ function createLiElement(text, classes = []) {
   element.classList = classes.join(' ')
   return element
 }
+
+liS.forEach((li) => li.addEventListener('dblclick', makeEditable))
+function makeEditable(e) {
+  e.target.addEventListener('blur', setEditedText)
+  e.target.setAttribute('contenteditable', 'true')
+  e.target.focus() // otherwise you have to reclick
+  const originalTextContent = e.target.textContent
+
+  function setEditedText(blurEvent) {
+    let category = getCategory(blurEvent)
+    console.log(blurEvent.target.textContent)
+    if (category === 'to-do') category = 'todo'
+    parsedTasks[category].splice(
+      parsedTasks[category].indexOf(originalTextContent),
+      1,
+      blurEvent.target.textContent
+    )
+    localStorage.setItem('tasks', JSON.stringify(parsedTasks))
+  }
+}
