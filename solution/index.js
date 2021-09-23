f07fb458c740c00db9fa38caf1ef3d21b18130af
romans1995 <cboby26@gmail.com>
2021-09-23 22:49:10 +0300

    working on my search

--- solution/index.js
+++ solution/index.js
@@ -1,71 +1,76 @@
-let Tasks = {"todo":[],"inprogress":[],"done":[]};
-
-
-// document.getElementById('pe').addEventListener('dblclick',()=>{
-//     let old = document.getElementById('pe')
-// let newinput = document.createElement('input');
-//     old.replaceWith(newinput);
-//  //    document.getElementById('pe').appendChild(newinput);
-//  // document.getElementById('pe').style.visibility = "hidden" ;  
-//   })
+let Tasks = {"todo":[],"in-progress":[],"done":[]};
         
-let counterId = 0;      
+     
 const add= (idToAdd) =>{
     
-    let currValeu = document.getElementById(`${idToAdd}-input`).value;
-    if(currValeu === ""){
+    let currValue = document.getElementById(`${idToAdd}-input`).value;
+    // if input dont have value
+    if(currValue === ""){
         return alert("oops"); 
     }
+
+    Tasks[idToAdd].push(currValue);
+    // creating li and input elments
     let Li = document.createElement("li");
     let inputLi = document.createElement('input');
+    // making a uniq id 
     const currTaskId = `${idToAdd}-${Tasks[idToAdd].length}`;
     Li.setAttribute('id', currTaskId);
-    inputLi.setAttribute("id",`${currTaskId} + '-input'`);
-    inputLi.setAttribute("display","none");
+    inputLi.setAttribute("id",currTaskId + '-input');
     let ul = document.getElementsByClassName(`${idToAdd}-tasks`)[0];
-    Li.appendChild(document.createTextNode(currValeu));
+    Li.appendChild(document.createTextNode(currValue));
     ul.append(Li);
-    Tasks[idToAdd].push(currValeu);
+    ul.append(inputLi);
     localStorage.setItem("Tasks",JSON.stringify(Tasks));
     document.getElementById(`${idToAdd}-input`).value = '';
+    inputLi.setAttribute('id', currTaskId + '-input');
+    Li.addEventListener('dblclick', () => {
+        document.getElementById(currTaskId).style.display = 'none';
+        document.getElementById(currTaskId + '-input').style.display = 'list-item';
+        
+    });
+   
+    ///when blur
+    inputLi.addEventListener('blur',()=>{
+        document.getElementById(currTaskId).textContent = 
+        document.getElementById(currTaskId + '-input').value;
+        const foundIndex = Tasks[idToAdd].indexOf(document.getElementById(currTaskId).textContent);
+        Tasks[idToAdd][foundIndex] = document.getElementById(currTaskId + '-input').value;
+        document.getElementById(currTaskId).style.display = 'list-item';
+        document.getElementById(currTaskId + '-input').style.display = 'none';
+    });
+    inputLi.style.display = 'none';
+    // makes the value of li stay in the input 
+    Li.textContent = currValue;
+    inputLi.value = currValue;
 
+   
+    //     Li.setAttribute('id', `in-progress-${Tasks['in-progress'].length}`);
+    //     inputLi.setAttribute("id",`in-progress-${Tasks['in-progress'].length}` + '-input');
+    //    Tasks['in-progress'].push(Li.textContent);
+    //    document.getElementsByClassName('in-progress-tasks')[0].append(Li);
+    //    document.getElementsByClassName('in-progress-tasks')[0].append(inputLi);
+    // //   
+    //    Tasks[idToAdd].splice(Li.textContent);
+    //    console.log(Tasks);
+}
+const search = () =>{
+    
+ 
+let input = document.getElementById('myinput');
+let filter = input.value;
+const ul = document.getElementById('done-tasks');
+let lengthOfE = Tasks.done.length
+for(i = 0; i < lengthOfE; i++){
+    let li =Tasks['done'].getElementsByTagName('li')[i];
+    console.log(li[i])
+    let txtValue = li.textContent || li.innerText;
+    if(txtValue.toUpperCase().indexOf(filter) > -1){
+        li[i].style.display = "";
+    }else {
+        li[i].style.display = "none";
+    }
 }
 
-// const addInProgress = () =>{
-//     let currValeu = document.getElementById("add-in-progress-task").value;
-//     if(currValeu === ""){
-//         return alert("oops"); 
-//     }
-//     let Li = document.createElement("li");
-//     Li.setAttribute("id",counterId)
-//     let ul = document.getElementsByClassName("in-progress-tasks")[0];
-//     Li.appendChild(document.createTextNode(currValeu));
-//     ul.append(Li);
-//     Tasks.inprogress.push(currValeu);
-//     localStorage.setItem("Tasks",JSON.stringify(Tasks));
-//     document.getElementById("add-in-progress-task").value = '';
-//     console.log(localStorage);
-//     counterId++;
-// }
-
-// const addDone = () =>{
-//     let currValeu = document.getElementById("add-done-task").value;
-//     if(currValeu === ""){
-//         return alert("oops"); 
-//     }
-//     let Li = document.createElement("li");
-//     Li.setAttribute("id",counterId)
-//     let ul = document.getElementsByClassName("done-tasks")[0];
-//     Li.appendChild(document.createTextNode(currValeu));
-//     ul.append(Li);
-//     Tasks.done.push(currValeu);
-//     localStorage.setItem("Tasks",JSON.stringify(Tasks));
-//     document.getElementById("add-done-task").value = '';
-//     counterId++;
-// }
-
-// if(localStorage.Tasks) {
-//     tasks = JSON.parse(localStorage.Tasks);
-// }
-// console.log(Tasks)
-
+   
+}
