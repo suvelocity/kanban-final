68eadc338839b3df7ff2083309c9caa941cfc0eb
IdanHerman12 <idanh156@gmail.com>
2021-09-23 12:54:26 +0300

    did the double click event and saved to local storage

--- solution/index.js
+++ solution/index.js
@@ -1,4 +1,5 @@
 const localSave=localStorage.getItem("tasks")
+let existTaskText
 // console.log(localSave)
 let data
 if(localSave!=undefined){
@@ -18,14 +19,13 @@ else{
 //function that adds a task to the addTasks section
 function addNewTask({target}){
     const section=target.closest("section")
-    // console.log(target.id)
     const newTask=section.querySelector("input").value
     if(newTask===""){
         alert("cannot add empty task")
     }
     else{
         const sectionUl=section.querySelector("ul")
-        const newTaskEl=createLiElement(newTask)
+        const newTaskEl=createLiElement(newTask,{"dblclick":editTask})
         sectionUl.prepend(newTaskEl)
         section.querySelector("input").value=""
         addNewTaskData(target,newTask)
@@ -33,6 +33,8 @@ function addNewTask({target}){
     }
 
 }
+
+
 function addNewTaskData(target,newTask){
     if(target.id==="submit-add-to-do"){
         data.todo.push(newTask)
@@ -45,92 +47,82 @@ function addNewTaskData(target,newTask){
     }
 }
 
+
 function saveLocalData(){
     const save=JSON.stringify(data)
     localStorage.setItem("tasks",save)
 }
-// const list=document.getElementsByClassName("to-do-tasks")
-// const li=createLiElement(text)
-// list[0].appendChild(li)
-// document.getElementById("add-to-do-task").value=""
-// tasks.todo.push(text)
-// localStorage.setItem("todo",JSON.stringify(tasks))
-// }
 
+function editTask(targetTask){
+   existTaskText=targetTask.target.innerHTML
+   targetTask.target.contentEditable = true
+}
 
-
-
-
-
-
-//function that adds a task to the addTasks section
-// function addInProgress(text){
-//     const list=document.getElementsByClassName("in-progress-tasks")
-//     const li=createLiElement(text)
-//     list[0].appendChild(li)
-//     document.getElementById("add-in-progress-task").value=""
-//     // const lijs=JSON.stringify({"text":li.value})    
-//     // arrayOfToDoTasks.unshift(text)
-//     localStorage.setItem("InProgress",text)
-//     }
-
-// //function the adds a task to done tasks section 
-//     function addDoneTasks(text){
-//         const list=document.getElementsByClassName("done-tasks")
-//         const li=createLiElement(text)
-//         list[0].appendChild(li)
-//         document.getElementById("add-done-task").value=""
-//         localStorage.setItem("doneTasks",text)
-        // }
-
-
-
-    function createLiElement(text){
-        const li=document.createElement("li")
-    li.setAttribute("class","task")
-    li.append(text)
-    return li
+function outOfEditTask(targetTask){
+    targetTask.target.contentEditable = false
+    const targetTasksSection=targetTask.target.closest("section")
+    const targetTaskSubmit=targetTasksSection.querySelector("button")
+    // console.log(targetTask.target.innerText)
+    const innerText=targetTask.target.innerText
+    if(innerText===""){
+        alert("cannot edit to empty task")
+        targetTask.target.innerText=existTaskText
     }
+    else{
+        const targetTaskSubmitId=targetTaskSubmit.id
+        let dataKey
+        if(targetTaskSubmitId==="submit-add-to-do")dataKey="todo"
+        if(targetTaskSubmitId==="submit-add-in-progress")dataKey="in-progress"
+        if(targetTaskSubmitId==="submit-add-done")dataKey="done"
+        // console.log(targetTask.target.innerText)
+        updateTask(dataKey,targetTask.target.innerText)
+    }
+}
+function updateTask(dataKey,newTaskText){
+    arrayOfData=data[dataKey]
+          for(let i=0;i<arrayOfData.length;i++){
+              if(arrayOfData[i]===existTaskText){
+                  data[dataKey][i]=newTaskText
+                  alert("changed")
+                  saveLocalData()
+                  return
+              }
+          }
+}
     
+
+    function createLiElement(text,eventListeners={}){
+        const liEl=document.createElement("li")
+    liEl.setAttribute("class","task")
+    liEl.append(text)
+    const events=Object.keys(eventListeners)
+    for(let i=0;i<events.length;i++){
+        liEl.addEventListener(events[i],eventListeners[events[i]])  
+    }
+    return liEl
+    }
+
+    //shows existing tasks that are saved in the local storage upon refreshing/start of the page
     function generateExistsTasks(){
         for(let i=0;i<data.todo.length;i++){    
-        const newTaskEl=createLiElement(data.todo[i])
+        const newTaskEl=createLiElement(data.todo[i],{"dblclick":editTask,"blur":outOfEditTask})
         const sectionUl=document.querySelector("#main > section:nth-child(2) > div > ul")
         sectionUl.prepend(newTaskEl)
         }
+    
         for(let i=0;i<data["in-progress"].length;i++){    
-            const newTaskEl=createLiElement(data["in-progress"][i])
+            const newTaskEl=createLiElement(data["in-progress"][i],{"dblclick":editTask})
             const sectionUl=document.querySelector("#main > section:nth-child(3) > div > ul")
             sectionUl.prepend(newTaskEl)
             }
             for(let i=0;i<data.done.length;i++){    
-                const newTaskEl=createLiElement(data.done[i])
+                const newTaskEl=createLiElement(data.done[i],{"dblclick":editTask})
                 const sectionUl=document.querySelector("#main > section:nth-child(4) > div > ul")
                 sectionUl.prepend(newTaskEl)
                 }
     }
 
-// function addTaskEvent(){
-// const text=document.getElementById("add-to-do-task").value
-// if(text==="")alert("cannot add empty task")
-// else
-// addTask(text)
-// }
 
-
-// function inProgressEvent(){
-//     const text=document.getElementById("add-in-progress-task").value
-//     if(text==="")alert("cannot add empty task")
-//     else
-//     addInProgress(text)
-//     } 
-
-//     function doneTasksEvent(){
-//         const text=document.getElementById("add-done-task").value
-//         if(text==="")alert("cannot add empty task")
-//         else
-//         addDoneTasks(text)
-//         }
 
 
 
