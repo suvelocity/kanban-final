ebd98ded43e3b7feb7248db802c299d05542ac0d
gabbyMill <gabbymillatiner@gmail.com>
2021-09-23 16:10:03 +0300

    Alt functionality
    Made alt function with help of object keeping track of whether a key is pressed or not using keydown and keyup.
    
    All of code uses iteration to set eventListeners instead of delegating
    (read that it's lighter, not sure if it's better practice or not..)

--- solution/index.js
+++ solution/index.js
@@ -11,11 +11,14 @@ const parsedTasks = JSON.parse(localStorage.getItem('tasks'))
 // add better explenation here later
 function paintDomFromLocalStorage() {
   const neaterUlNames = []
-  uls.forEach((ul) => neaterUlNames.push(ul.className.split('-tasks')[0])) // this leaves to-do, in-progress, done
+  uls.forEach((ul) => neaterUlNames.push(ul.className.split('-tasks')[0]))
+  // this leaves nicer names to work with
   for (let key in parsedTasks) {
     for (let i = 0; i < neaterUlNames.length; i++) {
       if (key.split('-').join('') === neaterUlNames[i].split('-').join('')) {
+        // find the matching ul to the mathching key (todo & to-do)
         for (let j = 0; j < parsedTasks[key].length; j++) {
+          // iterate over the property in the parsed version of the local storage and put it on the DOM
           uls[i].append(createLiElement(parsedTasks[key][j], ['task']))
         }
       }
@@ -24,12 +27,81 @@ function paintDomFromLocalStorage() {
 }
 paintDomFromLocalStorage()
 
+const liS = document.querySelectorAll('li')
+liS.forEach((li) => li.addEventListener('mouseenter', altHandlerFunction))
+
+function altHandlerFunction(e) {
+  // make sure this only runs while cursor is on <li>
+  liS.forEach((li) =>
+    li.addEventListener('mouseleave', () => {
+      document.removeEventListener('keydown', addToObject)
+    })
+  )
+  const numberOptions = ['1', '2', '3']
+
+  function addToObject(altClickEvent) {
+    const keyPressedObject = {}
+    keyPressedObject[altClickEvent.key] = true
+    document.addEventListener('keydown', function second(secondClickEvent) {
+      // document.removeEventListener('keydown', second)
+      // works with and without the above line but with it
+      // you can't leave your finger on Alt and keep moving it around. Haven't decided which I like better yet.
+      if (
+        keyPressedObject['Alt'] &&
+        numberOptions.includes(secondClickEvent.key)
+      ) {
+        let category = getCategory(e)
+        if (category === 'to-do') category = 'todo'
+        parsedTasks[category].splice(
+          parsedTasks[category].indexOf(e.target.textContent),
+          1 // deletes the appropriate li from it's place in parsed version of local storage.
+        )
+        switch (secondClickEvent.key) {
+          case '1':
+            uls[0].prepend(e.target)
+            parsedTasks.todo.unshift(e.target.textContent)
+            break
+          case '2':
+            uls[1].prepend(e.target)
+            parsedTasks['in-progress'].unshift(e.target.textContent)
+            break
+          case '3':
+            uls[2].prepend(e.target)
+            parsedTasks.done.unshift(e.target.textContent)
+            break
+        }
+        localStorage.setItem('tasks', JSON.stringify(parsedTasks))
+      }
+    })
+    document.addEventListener('keyup', (e) => {
+      keyPressedObject[e.key] = false
+    }) // makes sure BOTH keys are simultaneously pressed.
+  }
+
+  if (e.type === 'mouseenter') {
+    document.addEventListener('keydown', addToObject)
+  }
+}
+
 buttons.forEach((button) => button.addEventListener('click', inputListener))
 
-function inputListener(e) {
+function getCategory(e) {
   const section = e.target.closest('section')
   // find out what category we're in:
-  const category = section.id.split('-section')[0]
+  return section.id.split('-section')[0]
+}
+
+function updateLocalStorage(category, input, typeOfContent) {
+  for (let key in parsedTasks) {
+    if (key.split('-').join('') === category.split('-').join('')) {
+      parsedTasks[key].unshift(input[typeOfContent])
+      localStorage.setItem('tasks', JSON.stringify(parsedTasks))
+    }
+  }
+}
+
+function inputListener(e) {
+  const category = getCategory(e)
   const input = document.querySelector(`#add-${category}-task`)
   // check to see if box contains real text:
   if (input.value.replace(/\s/g, '').length < 1) {
@@ -37,14 +109,9 @@ function inputListener(e) {
     return
   }
   const newLiElement = createLiElement(input.value, ['task'])
-  document.querySelector(`.${category}-tasks`).append(newLiElement)
-  for (let key in parsedTasks) {
-    if (key.split('-').join('') === category.split('-').join('')) {
-      parsedTasks[key].push(input.value)
-      localStorage.setItem('tasks', JSON.stringify(parsedTasks))
-    }
-  }
-  //   localStorage.setItem(category + Math.random(), input.value) // later generate a new id here
+  newLiElement.addEventListener('mouseenter', altHandlerFunction) // make sure you don't have to refresh
+  document.querySelector(`.${category}-tasks`).prepend(newLiElement)
+  updateLocalStorage(category, input, 'value')
 }
 
 function createLiElement(text, classes = []) {
