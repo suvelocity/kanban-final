7d386f9f5d6095ea820b64c1a6510e8179526a5b
ElayGelbart <elaygelbart@gmail.com>
2021-09-23 16:53:03 +0300

    Test #7 and Test #8
    can send data to the API and get data back from it

--- solution/index.js
+++ solution/index.js
@@ -1,6 +1,6 @@
 'use strict'
-let taskObj,resultObj;
-
+let taskObj, resultObj;
+const spaceAtEndRegex = /[\s]*$/g;
 const addEventListenerToDeleteButton = () => {
     const deleteButtons = document.querySelectorAll('.deleteButton');
     deleteButtons.forEach(element => {
@@ -19,26 +19,33 @@ const addEventListenerToTasks = () => {
             let oldinnerTaskText = element.getElementsByClassName("TaskTitle")[0].innerText;
             element.getElementsByClassName("TaskTitle")[0].contentEditable = true;
             element.getElementsByClassName("TaskTitle")[0].onblur = function () {
-                let newinnerTaskText = element.getElementsByClassName("TaskTitle")[0].innerText;
+                let newinnerTaskText = element.getElementsByClassName("TaskTitle")[0].innerText.replace(spaceAtEndRegex, '');
                 deleteNChangeTask(oldinnerTaskText, newinnerTaskText);
+                localStorage.setItem("tasks", JSON.stringify(taskObj));
             }
         })
         element.addEventListener("click", function () {
             try { document.querySelector("#selectedTask").id = ""; }
             catch { element.id = "selectedTask"; }
             element.id = "selectedTask";
-            window.addEventListener("keydown", function (e) {
+            window.addEventListener("keydown", function presskey (e) {
                 if (e.key === "Alt") {
-                    window.addEventListener("keydown", function (e) {
+                    window.addEventListener("keydown", function preesnumber (e) {
                         if (e.key === "1") {
+                            window.removeEventListener("keydown",preesnumber);
+                            window.removeEventListener("keydown",presskey);
                             let innerTaskText = element.innerText;
                             deleteNAddToOtherTask("todo", innerTaskText);
                         }
                         else if (e.key === "2") {
+                            window.removeEventListener("keydown",preesnumber);
+                            window.removeEventListener("keydown",presskey);
                             let innerTaskText = element.innerText;
                             deleteNAddToOtherTask("in-progress", innerTaskText);
                         }
                         else if (e.key === "3") {
+                            window.removeEventListener("keydown",preesnumber);
+                            window.removeEventListener("keydown",presskey);
                             let innerTaskText = element.innerText;
                             deleteNAddToOtherTask("done", innerTaskText);
                         }
@@ -51,8 +58,8 @@ const addEventListenerToTasks = () => {
 }
 
 const addTask = (taskType, id) => {
-    const taskValue = document.getElementById(id).value;
-    if (!document.getElementById(id).value.replace(/\s/g, '')) { return alert('s') }
+    const taskValue = document.getElementById(id).value.replace(spaceAtEndRegex, '');
+    if (!document.getElementById(id).value.replace(/\s/g, '')) { return alert('enter Valid Task') }
     document.getElementById(id).value = "";
     taskObj[taskType].unshift(taskValue);
     postTasks();
@@ -119,6 +126,7 @@ const deleteNChangeTask = (oldTaskText, newTaskText) => {
             if (taskObj[taskarray][i] == oldTaskText) {
                 taskObj[taskarray].splice(i, 1);
                 taskObj[taskarray].unshift(newTaskText)
+                localStorage.setItem("tasks", JSON.stringify(taskObj));
             }
         }
     }
@@ -164,6 +172,40 @@ const searchByQuery = (query) => {
     return resultObj;
 }
 
+const getDataFromAPI = async () => {
+    document.getElementById("loaderD").innerHTML = `<img src="./Images/loading.gif" alt="loader" class="loader" id="loader">`;
+    const response = await fetch("https://json-bins.herokuapp.com/bin/614ad65e4021ac0e6c080c06");
+    if(!response.ok){
+        alert(response.status);
+    }
+    const responseObj = await response.json();
+    taskObj = responseObj.tasks;
+    localStorage.setItem("tasks", JSON.stringify(taskObj));
+    document.getElementById("loaderD").innerHTML = ``;
+    postTasks();
+}
+
+const postDataToAPI = async () => {
+    document.getElementById("loaderD").innerHTML = `<img src="./Images/loading.gif" alt="loader" class="loader" id="loader">`;
+    const tasksobjtoAPI  = {tasks: taskObj} ;
+    const response = await fetch("https://json-bins.herokuapp.com/bin/614ad65e4021ac0e6c080c06", {
+        method: "PUT",
+        headers: {
+        //     "Access-Control-Allow-Origin": "origin-list",
+            "Content-Type": "application/json",
+            "Accept" : "application/json",
+        },
+        body: JSON.stringify(tasksobjtoAPI),
+    });
+    document.getElementById("loaderD").innerHTML = ``;
+    if(!response.ok){
+        alert(response.status);
+    }
+}
+
+document.getElementById("search").addEventListener("keyup", postTasksforquery);
+document.getElementById("load-btn").addEventListener("click", getDataFromAPI);
+document.getElementById("save-btn").addEventListener("click", postDataToAPI);
 if (localStorage.getItem("tasks")) {
     taskObj = JSON.parse(localStorage.getItem("tasks"));
     postTasks();
@@ -177,4 +219,3 @@ else {
     postTasks();
     localStorage.setItem("tasks", JSON.stringify(taskObj));
 }
-document.getElementById("search").addEventListener("keyup",postTasksforquery)
