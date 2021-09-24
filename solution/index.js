fc9b39167a8daee39b9e6b6a18a3e606cf836f82
romans1995 <cboby26@gmail.com>
2021-09-24 14:34:15 +0300

    values go from done to todo if you mouseOver + press "ALT" + 1,2,3

--- solution/index.js
+++ solution/index.js
@@ -1,6 +1,6 @@
 let Tasks = {"todo":[],"in-progress":[],"done":[]};
-        
-     
+      
+  
 const add= (idToAdd) =>{
     
     let currValue = document.getElementById(`${idToAdd}-input`).value;
@@ -43,8 +43,38 @@ const add= (idToAdd) =>{
     // makes the value of li stay in the input 
     Li.textContent = currValue;
     inputLi.value = currValue;
-
-   
+    // makes values go from done to todo if you mouseOver + press "ALT" + 1,2,3
+    let mouseHover = false;
+    let onAlt = false;
+    
+    Li.addEventListener("mouseover",() =>{
+      mouseHover = true;
+     });
+     Li.addEventListener("mouseleave",() =>{
+      mouseHover = false;
+     });
+     document.onkeydown = (e) =>{
+        var key = e.key 
+         if(key === 'Alt'){
+            onAlt = true;
+         } 
+         }
+         document.onkeyup = (e) =>{
+            var key = e.key 
+             if(key === 'Alt'){
+                onAlt = false;
+             }
+             }
+    window.onkeydown = (event) =>{
+        var key = event.key 
+           if(mouseHover && key === "1" && onAlt){
+            document.getElementsByClassName('todo-tasks')[0].append(Li);
+           }else if (mouseHover && key === "2"&& onAlt){
+            document.getElementsByClassName('in-progress-tasks')[0].append(Li);
+           }else if(mouseHover && key === "3"&& onAlt){
+            document.getElementsByClassName('done-tasks')[0].append(Li);
+           }
+        // document.getElementsByClassName('todo-tasks')[0].append(Li);     
     //     Li.setAttribute('id', `in-progress-${Tasks['in-progress'].length}`);
     //     inputLi.setAttribute("id",`in-progress-${Tasks['in-progress'].length}` + '-input');
     //    Tasks['in-progress'].push(Li.textContent);
@@ -55,22 +85,25 @@ const add= (idToAdd) =>{
     //    console.log(Tasks);
 }
 const search = () =>{
-    
- 
 let input = document.getElementById('myinput');
 let filter = input.value;
+const returnLi = {"todo":[],"in-progress":[],"done":[]};
+returnLi.done = Tasks.done.filter(currValue => {
+    let isFound = false;
+    Object.values()
+});
+
 const ul = document.getElementById('done-tasks');
 let lengthOfE = Tasks.done.length
 for(i = 0; i < lengthOfE; i++){
     let li =Tasks['done'].getElementsByTagName('li')[i];
-    console.log(li[i])
+    
     let txtValue = li.textContent || li.innerText;
     if(txtValue.toUpperCase().indexOf(filter) > -1){
         li[i].style.display = "";
     }else {
         li[i].style.display = "none";
     }
+    }
 }
-
-   
 }
