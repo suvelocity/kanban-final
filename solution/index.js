c370df70919fb48d2a2a15c761a3f6c1a2ff2efe
eitam1400 <eitamshalom@gmail.com>
2021-09-24 23:09:15 +0300

    features & bug fixes
    double click editable, errors handlers, searchbar
    
    Co-Authored-By: Cyber4sPopo <69596366+cyber4spopo@users.noreply.github.com>

--- solution/index.js
+++ solution/index.js
@@ -20,7 +20,6 @@ const search = document.getElementById('search'); //search bar input section hol
 
 //---------------------------------------------------------EVENT LISTENERS---------------------------------------------------------------------//
 
-
  
 // add to do task content to matchin list/s  & saving in local storage //
 addToDoButton.addEventListener('click', () => {
@@ -28,74 +27,160 @@ addToDoButton.addEventListener('click', () => {
     liGenerator( toDoInput, toDoTasksList );
        
     
-});
+}, { passive: true });
+
+toDoInput.addEventListener('keydown' , (e) => {
+
+    if(e.key === "Enter") liGenerator( toDoInput, toDoTasksList );     
+    
+}, { passive: true });
+
+
+
+
 
 // add in progress task content to matchin list/s  & saving in local storage //
 addInProgressButton.addEventListener('click', () => {
 
     liGenerator( inProgressInput, inProgressTasksList );
-    localStorage.setItem(`${listElement}, ${inProgressInput}`);
-    
 });
+
+inProgressInput.addEventListener('keydown' , (e) => {
+
+    if(e.key === "Enter") liGenerator( inProgressInput, inProgressTasksList );     
+    
+}, { passive: true });
+
+
+
 
 
 // add done task cntent to matchin list/s & saving in local storage //
-addDoneButton.addEventListener('click', () => {
+addDoneButton.addEventListener('click' , () => {
 
-    liGenerator( doneInput, DoneTasksList );
-       
+    liGenerator( doneInput, DoneTasksList );     
     
-});
+}, { passive: true });
 
-/** 
+doneInput.addEventListener('keydown' , (e) => {
 
-[addToDoButton, addInProgressButton, addDoneButton].forEach( addButton =>
-    { 
-    addButton.addEventListener('click', () => {
-    switch(addButton)
-    {
-        case addToDoButton:
-         liGenerator( toDoInput, toDoTasksList );
-         break;
+    if(e.key === "Enter") liGenerator( doneInput, DoneTasksList );     
+    
+}, { passive: true });
 
-        case addInProgressButton:
-         liGenerator( inProgressInput, inProgressTasksList );
-         break;
 
-        case addDoneButton:
-         liGenerator( doneInput, DoneTasksList );
-         break;
-    }
-  })
-});
+// try
+// {
+    var UserContentInputArray = []; //public array of user inputs li
+    const listItems = document.querySelectorAll('ul'); //selecting lists
+    var isHover = false;
 
-**/
+    const observer = new MutationObserver( (mutations) => { //observing for changes in the list
+        
+         mutations.forEach( function (mutation){
 
-try
-{
-   const listItems = document.querySelectorAll('.li-task');
-   for( const listItem of listItems)
-   {
-      // listItem.addEventListener( 'dblclick', listItem. )
-   }
-}
-catch(err)
-{
-    throw(`step up son, you've got another error GOTCH'U 😈`);
-}
+            
+
+            if(mutation.addedNodes.length) //if list item got added === true
+            {                
+                    const liTask = mutation.addedNodes[0].childNodes[0];
+
+                    //!!~~**DONT FORGET TO UPDATE WHEN ITEM DELETED**~~!!//
+                    UserContentInputArray.push(liTask); //pushin newcontent to array to store its value
+
+                    console.log(UserContentInputArray)
+                    liTask.addEventListener('click', (e) => { //event when user click 4 times in a row it removes the specific list item from the specific lsit
+                        if(e.detail === 4)
+                        {
+                          e.target.parentElement.remove() //removing its whole from list
+                          UserContentInputArray.splice(UserContentInputArray.indexOf(e.target),1) //removing it from the user input array as well
+                          console.log(UserContentInputArray)
+                        }
+                    }, { passive: true });
+
+                    liTask.addEventListener( 'dblclick', () => { //add event listener to the first child every time 
+
+                        liTask.setAttribute("contenteditable", true); //make it editable
+                        liTask.classList.remove('hover') // remove element  hovering effect when doublclicked 
+                        liTask.classList.add('no_selection') // remove element selection backgroun color when doublclicked 
+                        liTask.nextSibling.classList.add('no_selection')
+    
+                          liTask.addEventListener( 'keydown', (e) => { //when Enter hit prevent its default
+                                if(e.key === "Enter") 
+                                {
+                                   e.preventDefault()
+                                }
+                            }); 
+                    }); 
+
+
+                    liTask.addEventListener( 'blur', () => { //vent when user loses focus off the editable field
+                        liTask.setAttribute("contenteditable", false); //return it to its initial state
+                        liTask.classList.add('hover');
+                        liTask.classList.remove('no_selection')
+                        liTask.nextSibling.classList.remove('no_selection')
+                        isHover = false;
+                    }, { passive: true });
+
+    
+                    liTask.addEventListener('keyup', (e) => { // detectin keyboard events are only on input type elements so i detect it through window object
+                        console.log(isHover)
+                        if((e.altKey || e.getModifierState('AltGraph')) )
+                        {
+                            console.log('lol')
+                        }
+                    });
+    
+                    liTask.addEventListener('mouseover', (e) => {
+                        isHover = true;
+                        //liTask.matches(':hover')
+                    });                   
+            }
+        });       
+            
+    });
+        
+        
+    listItems.forEach((list) => { //observing changes for every list
+     observer.observe( list, {childList: true }); // observing for list items which being added
+    });
+
 
 
 //Search bar event//
-search.addEventListener('keypress', e => {
-    if(e.key === "Enter")
-    {
-        if(isElementEmpty(search)){} //checking if field empty and assignin error class style if it is.
-        else{
-          search.classList.remove('error'); //removing error style class. if its not empty and doesnt have the error attached to it it will just keep going cuz no error cass in it
+search.addEventListener('keydown', e => {
+
+
+    if(e.key !== "Enter"){
+        if(isElementEmpty(search)){
+           //checking if field empty and assignin error class style if it is.
+        } else {
+          [search, inProgressInput, toDoInput, doneInput].forEach(el => el.classList.remove("error")); //removing error style class. if its not empty and doesnt have the error attached to it it will just keep going cuz no error cass in it
         }
     }
 
-})
+    let searchInput = search.value.toLowerCase().split(' ');
+    if(e.key === "Escape" && searchInput[0] != '') 
+    {
+        console.log( searchInput[searchInput.length - 1].pop() );
+    }
+
+    //if(search.value = /a-z/) //regex check for valid input
+    if(searchInput[0] != '') 
+    {
+        console.log(searchInput);
+        //console.log(UserContentInputArray);
+    }
+
+    //else
+
+    //console.log(UserContentInputArray)
+
+    UserContentInputArray.forEach(() => {
+        searchInput
+    });
+    
+}, { passive: true })
 
 //---------------------------------------------------------FREE AREA---------------------------------------------------------------------//
 
@@ -121,6 +206,8 @@ function isElementEmpty(el)
 //function gets an input element and sets an li element to the coresponsing section of the task//
 function liGenerator( inputElement, arrayOfListElementsByClass )
 {
+    const inputsArray = [toDoInput, doneInput, inProgressInput, search]; //input fields arrays
+
     if ( isElementEmpty(inputElement) || arrayOfListElementsByClass.length === 0 ) 
     {
         setTimeout(() => {
@@ -129,8 +216,9 @@ function liGenerator( inputElement, arrayOfListElementsByClass )
     }
     else
     {
-      inputElement.classList.remove('error'); // if not empty remove error style attribute
-      const userInputSection = elCreator('span', [inputElement.value], ['li-task']); //section of user input only creator
+      inputsArray.forEach( inputField => inputField.classList.remove('error'));  // if not empty (& and submitted one form) remove error style attribute to all input fieldds 
+
+      const userInputSection = elCreator('span', [inputElement.value], ['li-task', 'hover']); //section of user input only creator
       const liEl = elCreator( 'li', [userInputSection], ['list'], {} ); //creating and inserting li el to list from user input
 
       
@@ -150,7 +238,18 @@ function liGenerator( inputElement, arrayOfListElementsByClass )
       //apend FULLDATE to list!
       eleDOMAppender(liEl, fulldateEl); //apending full date to the task list
       
-      arrayOfListElementsByClass.forEach( listElement =>  eleDOMAppender( listElement , liEl )); //sets the value of the input to the currrent state of it
+      arrayOfListElementsByClass.forEach( listElement => {
+
+            if(listElement.firstElementChild === 'null') //if it doesnt have a  child, append it.
+            {
+                eleDOMAppender( listElement , liEl ); //sets the value of the input to the currrent state of it
+            }
+            else{ //if it does  have a child insert the generated li from user to the top of the ul
+               listElement.insertBefore(liEl, listElement.firstChild);
+            }
+
+        });
+
       inputElement.value = ''; //resetin the value of input field after submittion
     } 
 } 
@@ -186,11 +285,6 @@ function eleDOMAppender( destintionEle, currentEle ){
     {
         throw(`there was an error with your element. please make sure it's not Null!`);
     }
-}
-
-function tagElementConverter( destinationTagElementName, currentTagElement)
-{
-
 }
 
 
