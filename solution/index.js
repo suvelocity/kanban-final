2980a5666dde7c7a699dbec0caf4202f5e827cfc
romans1995 <cboby26@gmail.com>
2021-09-25 14:10:29 +0300

    fixed localstorage and search add new global mapper

--- solution/index.js
+++ solution/index.js
@@ -1,141 +1,181 @@
-let Tasks = {"todo":[],"in-progress":[],"done":[]};
-      
-  
-const add = (idToAdd) =>{
-    
-    let currValue = document.getElementById(`add-${idToAdd}-task`).value;
-    // if input dont have value
-    if(currValue === ""){
-        return alert("oops"); 
-    }
-    if(idToAdd === 'to-do'){
-        idToAdd = 'todo';
-    }
-    Tasks[idToAdd].unshift(currValue);
-    // creating li and input elments
-    let Li = document.createElement("li");
-    let inputLi = document.createElement('input');
-    // making a uniq id 
-    const currTaskId = `${idToAdd}-${Tasks[idToAdd].length}`;
-    Li.setAttribute('id', currTaskId);
-    Li.setAttribute('class', 'task');
-    inputLi.setAttribute("id",currTaskId + '-input');
-    console.log(idToAdd)
-    let ul = document.getElementsByClassName(`${idToAdd}-tasks`)[0];
-    Li.appendChild(document.createTextNode(currValue));
-    ul.append(Li);
-    ul.append(inputLi);
-    localStorage.setItem("tasks",JSON.stringify(Tasks));
-    if(idToAdd === 'todo'){
-        idToAdd = 'to-do'
-    }
-    document.getElementById(`add-${idToAdd}-task`).value = '';
-    inputLi.setAttribute('id', currTaskId + '-input');
-    Li.addEventListener('dblclick', () => {
-        document.getElementById(currTaskId).style.display = 'none';
-        document.getElementById(currTaskId + '-input').style.display = 'list-item';
-        
-    });
+let Tasks = { 'todo': [], 'in-progress': [], 'done': [] };
+const mapper = {
+    '1': 'todo',
+    '2': 'in-progress',
+    '3': 'done'
+}
+
+const load = () => {
+    if (localStorage.tasks) {
+        Tasks = JSON.parse(localStorage.tasks);
+
+        Object.keys(Tasks).forEach((categoryKey) => {
+            const updatedCategory = categoryKey === 'todo' ?
+                'to-do-tasks' : `${categoryKey}-tasks`;
+            const updatedId = categoryKey === 'todo' ?
+                'to-do' : categoryKey;
+
+            Tasks[categoryKey].forEach((currTask, i) => {
+                const ul = document.getElementById(updatedCategory);
+                const li = document.createElement('li');
+                const inputLi = document.createElement('input');
+                li.setAttribute('id', `${updatedId}-${i + 1}`);
+                inputLi.setAttribute('id', `${updatedId}-${i + 1}-input`);
+                li.setAttribute('class', 'task');
+                li.textContent = currTask;
+                inputLi.value = currTask;
+                inputLi.style.display = 'none';
+
+                ul.appendChild(li);
+                ul.appendChild(inputLi);
+                
+                // li.addEventListener('dblclick', () => dblClickFunction(`${updatedId}-${i + 1}`));
    
     ///when blur
-    inputLi.addEventListener('blur',()=>{
-        document.getElementById(currTaskId).textContent = 
-        document.getElementById(currTaskId + '-input').value;
-        if(idToAdd === 'to-do'){
-            idToAdd = 'todo';
-        }
-        const foundIndex = Tasks[idToAdd].indexOf(document.getElementById(currTaskId).textContent);
-        Tasks[idToAdd][foundIndex] = document.getElementById(currTaskId + '-input').value;
-        document.getElementById(currTaskId).style.display = 'list-item';
-        document.getElementById(currTaskId + '-input').style.display = 'none';
-    });
-    inputLi.style.display = 'none';
-    // makes the value of li stay in the input 
-    Li.textContent = currValue;
-    inputLi.value = currValue;
-    // makes values go from done to todo if you mouseOver + press "ALT" + 1,2,3
-    let mouseHover = false;
-    let onAlt = false;
-    
-    Li.addEventListener("mouseover",() =>{
-      mouseHover = true;
-     });
-     Li.addEventListener("mouseleave",() =>{
-      mouseHover = false;
-     });
-     document.onkeydown = (e) =>{
-        var key = e.key 
-         if(key === 'Alt'){
-            onAlt = true;
-         } 
-         }
-         document.onkeyup = (e) =>{
-            var key = e.key 
-             if(key === 'Alt'){
-                onAlt = false;
-             }
-             }
-    window.onkeydown = (event) =>{
-        var key = event.key 
-           if(mouseHover && key === "1" && onAlt){
-            Li.setAttribute('id', `todo-${Tasks['todo'].length+1}`);
-            inputLi.setAttribute("id",`todo-${Tasks['todo'].length+1}` + '-input');
-            document.getElementsByClassName('todo-tasks')[0].append(Li);
-            document.getElementsByClassName('todo-tasks')[0].append(inputLi);
-
-           }else if (mouseHover && key === "2"&& onAlt){
-            Li.setAttribute('id', `in-progress-${Tasks['in-progress'].length+1}`);
-            inputLi.setAttribute("id",`in-progress-${Tasks['in-progress'].length+1}` + '-input');
-            document.getElementsByClassName('in-progress-tasks')[0].append(Li);
-            document.getElementsByClassName('in-progress-tasks')[0].append(inputLi);
-
-           }else if(mouseHover && key === "3"&& onAlt){
-            Li.setAttribute('id', `done-${Tasks['done'].length+1}`);
-            inputLi.setAttribute("id",`done-${Tasks['done'].length+1}` + '-input');
-            document.getElementsByClassName('done-tasks')[0].append(Li);
-            document.getElementsByClassName('done-tasks')[0].append(inputLi);
-           }
-        }
+    // inputLi.addEventListener('blur', () =>  blurFunction(`${updatedId}-${i + 1}`));*/
+});
+});
+} else {
+localStorage.tasks = JSON.stringify(Tasks);
 }
-// end of add function 
+}
+
+const dblClickFunction = (currTaskId) => {
+document.getElementById(currTaskId).style.display = 'none';
+document.getElementById(currTaskId + '-input').style.display = 'list-item';
+document.getElementById(currTaskId + '-input').focus();
+}
+
+const blurFunction = (currTaskId) => {
+const idForUse = currTaskId.substr(0, currTaskId.lastIndexOf('-')) === 'to-do' ?
+'todo' : currTaskId.substr(0, currTaskId.lastIndexOf('-'))
+
+const foundId = Tasks[idForUse].indexOf(document.getElementById(currTaskId).textContent);
+Tasks[idForUse][foundId] = document.getElementById(currTaskId + '-input').value;
+localStorage.tasks = JSON.stringify(Tasks);
+
+document.getElementById(currTaskId).textContent =
+document.getElementById(currTaskId + '-input').value;
+document.getElementById(currTaskId).style.display = 'list-item';
+document.getElementById(currTaskId + '-input').style.display = 'none';
+}
+
+const add = (idToAdd) => {
+let currValue = document.getElementById(`add-${idToAdd}-task`).value;
+
+// if input dont have value
+if (currValue === "") {
+return alert("Please enter a value");
+}
+
+// Reset the input after adding the task
+document.getElementById(`add-${idToAdd}-task`).value = '';
+
+// creating li and input elments
+const Li = document.createElement("li");
+const inputLi = document.createElement('input');
+
+idToAdd === 'to-do' ?
+Tasks['todo'].push(currValue) :
+Tasks[idToAdd].push(currValue);
+localStorage.tasks = JSON.stringify(Tasks);
+
+// making a unique id 
+const currTaskId = `${idToAdd}-${Tasks[idToAdd === 'to-do' ? 'todo' : idToAdd].length}`;
+
+Li.setAttribute('id', currTaskId);
+Li.setAttribute('class', 'task');
+inputLi.setAttribute('id', `${currTaskId}-input`);
+
+const ul = document.getElementById(`${idToAdd}-tasks`);
+
+Li.textContent = currValue;
+inputLi.value = currValue;
+inputLi.style.display = 'none';
+
+ul.appendChild(Li);
+ul.appendChild(inputLi);
+
+Li.addEventListener('dblclick', () => dblClickFunction(currTaskId));
+
+///when blur
+inputLi.addEventListener('blur', () =>  blurFunction(currTaskId));
+
+// makes values go from done to todo if you mouseOver + press "ALT" + 1,2,3
+let mouseHover = false;
+let onAlt = false;
+
+Li.addEventListener("mouseover", () => {
+mouseHover = true;
+});
+Li.addEventListener("mouseleave", () => {
+mouseHover = false;
+});
+
+document.onkeydown = (e) => {
+if (e.key === 'Alt') {
+    onAlt = true;
+}
+}
+document.onkeyup = (e) => {
+if (e.key === 'Alt') {
+    onAlt = false;
+}
+}
+
+window.onkeydown = (e) => {
+if (mouseHover && ['1','2','3'].includes(e.key) && onAlt) {
+    const currentId = `${mapper[e.key]}-${Tasks[mapper[e.key]].length + 1}`;
+
+    Tasks[idToAdd].splice(Tasks[idToAdd].indexOf(Li.textContent), 1);
+    Tasks[mapper[e.key]].push(Li.textContent);
+
+    Li.setAttribute('id', currentId);
+    inputLi.setAttribute("id", `${currentId}-input`);
+    document.getElementById(`${mapper[e.key]}-tasks`).append(Li);
+    document.getElementById(`${mapper[e.key]}-tasks`).append(inputLi);
+
+    Li.removeEventListener('dblclick', () => dblClickFunction(currentId));
+    Li.addEventListener('dblclick', () => dblClickFunction(currentId));
+
+    inputLi.removeEventListener('blur', () => blurFunction(currentId));
+    inputLi.addEventListener('blur', () => blurFunction(currentId));
+}
+}
+// document.getElementsByClassName('todo-tasks')[0].append(Li);     
+//     Li.setAttribute('id', `in-progress-${Tasks['in-progress'].length}`);
+//     inputLi.setAttribute("id",`in-progress-${Tasks['in-progress'].length}` + '-input');
+//    Tasks['in-progress'].push(Li.textContent);
+//    document.getElementsByClassName('in-progress-tasks')[0].append(Li);
+//    document.getElementsByClassName('in-progress-tasks')[0].append(inputLi);
+// //   
+//    Tasks[idToAdd].splice(Li.textContent);
+//    console.log(Tasks);
+}
 const search = () => {
-    let input = document.getElementById('search');
-    let filtered = input.value;
+let input = document.getElementById('search');
+let filtered = input.value;
 
-    Array.from(document.getElementsByTagName('li')).forEach(currElement => {
-        if (currElement.textContent.match(filtered)) {
-            currElement.style.display = 'list-item';
-        } else {
-            currElement.style.display = 'none';
-        }
-    });
+Array.from(document.getElementsByTagName('li')).forEach(currElement => {
+if (currElement.textContent.match(filtered)) {
+    currElement.style.display = 'list-item';
+} else {
+    currElement.style.display = 'none';
+}
+});
 }
 
+load();
 
+// const ul = document.getElementById('done-tasks');
+// let lengthOfE = Tasks.done.length
+// for(i = 0; i < lengthOfE; i++){
+//     let li =Tasks['done'].getElementsByTagName('li')[i];
 
-// for(let i = 0; i < 3;++i){
-//     returnLi[mapper[i]] = Tasks[mapper[i]].filter(LiValue => {
-//         let isFound = false;
-//         Object.values(LiValue).forEach(currValue =>{
-//            if(currValue.toLowerCase().match(filtered)){
-//             isFound = true;
-//            } 
-//         });
-//         return isFound;
-//     });
-//     returnLi[mapper[i]] = Tasks[mapper[i]].filter(currLi =>{
-//         let result = currLi.match(filtered);
-//         console.log(result)
-//         console.log(document.getElementsByTagName('li'));
-//         if(result !== document.getElementsByTagName('li')[i].data && result === null){
-//             console.log(result)
-//             document.getElementsByTagName('li')[i].style.visibility = 'hidden'
-//         }
-//         if(filtered === ''){
-//             document.getElementsByTagName('li')[i].style.visibility = 'visible';
-//         }
-//     })
-    
+//     let txtValue = li.textContent || li.innerText;
+//     if(txtValue.toUpperCase().indexOf(filter) > -1){
+//         li[i].style.display = "";
+//     }else {
+//         li[i].style.display = "none";
+//     }
 // }
-
-
