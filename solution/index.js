df9d4adcbdf971348138b453892f692f8546072a
Roy Shemesh <89579074+RoyShemesh@users.noreply.github.com>
2021-09-25 14:31:34 +0300

    Add move tasks function

--- solution/index.js
+++ solution/index.js
@@ -1,27 +1,32 @@
 //function that check the user's name
 function checkName()
 {
-    let name=localStorage.getItem("name");
-    let lists;
+    //let name=localStorage.getItem("name");
+    let name=localStorage.getItem("tasks");
+    let tasks;
     if(name===null)
     {
-        lists={
+        tasks={
             "todo": [],
             "in-progress": [],
             "done": [],
         };
-        name=prompt("What is your name?");
-        localStorage.setItem("lists",JSON.stringify(lists));
-        localStorage.setItem("name",name);
+        //name=prompt("What is your name?");
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+        //localStorage.setItem("name",name);
     }
-    document.getElementById("page-title").innerText=name+"'s kanban";
-    lists=JSON.parse(localStorage.getItem("lists"));
-    for (let list in lists) {
-        lists[list].forEach(text => {
-            let task=createElement("li",[],["list-group-item"])
-            task.innerText=JSON.parse(text);
+    //document.getElementById("page-title").innerText=name+"'s kanban";
+    tasks=JSON.parse(localStorage.getItem("tasks"));
+    for (let list in tasks) {
+        tasks[list].forEach(text => {
+            let task=createElement("li",[],["list-group-item","task" ])
+            task.innerText=text;
             document.getElementById(list).append(task);
             task.addEventListener('dblclick', changeTask);
+            task.addEventListener('mouseover', moveTask);
+            task.addEventListener('mouseout', moveTaskEnd);
+
+
         });
     }
 }
@@ -45,8 +50,11 @@ function createElement(tagName ,children = [], classes = [], attributes = {}) {
 // create a new task and put it in his list 
 function createTask(event)
 {
-    let task =createElement("li",[],["list-group-item"],);
+    let task =createElement("li",[],["list-group-item","task"],);
     task.addEventListener('dblclick', changeTask);
+    task.addEventListener('mouseover', moveTask);
+    task.addEventListener('mouseout', moveTaskEnd);
+
     let section=event.target.parentElement.id;
     let ul,text;
     switch(section)
@@ -60,11 +68,11 @@ function createTask(event)
             }
             task.innerText=text;
             ul=document.getElementById("todo");
-            ul.append(task); 
+            ul.insertBefore(task,ul.firstChild); 
             document.getElementById("add-to-do-task").value="";
-            list=JSON.parse(localStorage.getItem("lists"));//push the task to local storage
-            list["todo"].push(JSON.stringify(text));
-            localStorage.setItem("lists",JSON.stringify(list));
+            list=JSON.parse(localStorage.getItem("tasks"));//push the task to local storage
+            list["todo"].unshift(text);
+            localStorage.setItem("tasks",JSON.stringify(list));
             break;
         case "2":
             text=document.getElementById("add-in-progress-task").value;
@@ -75,11 +83,11 @@ function createTask(event)
             }
             task.innerText=text;
             ul=document.getElementById("in-progress");
-            ul.append(task);
+            ul.insertBefore(task,ul.firstChild); 
             document.getElementById("add-in-progress-task").value="";
-            list=JSON.parse(localStorage.getItem("lists"));//push the task to local storage
-            list["in-progress"].push(JSON.stringify(text));
-            localStorage.setItem("lists",JSON.stringify(list));
+            list=JSON.parse(localStorage.getItem("tasks"));//push the task to local storage
+            list["in-progress"].unshift(text);
+            localStorage.setItem("tasks",JSON.stringify(list));
             break;
         case "3":
             text=document.getElementById("add-done-task").value;
@@ -90,14 +98,12 @@ function createTask(event)
             }
             task.innerText=text;
             ul=document.getElementById("done");
-            ul.append(task);
+            ul.insertBefore(task,ul.firstChild); 
             document.getElementById("add-done-task").value="";
-            list=JSON.parse(localStorage.getItem("lists"));//push the task to local storage
-            list["done"].push(JSON.stringify(text));
-            localStorage.setItem("lists",JSON.stringify(list));
+            list=JSON.parse(localStorage.getItem("tasks"));//push the task to local storage
+            list["done"].unshift(text);
+            localStorage.setItem("tasks",JSON.stringify(list));
             break;
-        default:
-            console.log("deafault")
     }
 }
 //function that check if task is empty
@@ -115,10 +121,11 @@ function changeTask(event)
     let list=task.parentElement.id;
     let text=task.innerText;
     list=document.getElementById(list);
-    const change=createElement("input",[],["form__input"],{value:text,onfocusout:"changeTaskComplete()"});
+    const change=createElement("input",[],["form__input"],{value:text,onblur:"changeTaskComplete()"});
     list.append(change);
     list.replaceChild(change,task);
     deleteTaskByText(text,list.id);
+    console.log("here")
     
 }
 //complete the change task
@@ -134,38 +141,72 @@ function changeTaskComplete(event)
         alert("Task must has info");
         throw("Task must has info");
     }
-    let task =createElement("li",[],["list-group-item"],);
+    let task =createElement("li",[],["list-group-item","task"],);
     task.addEventListener('dblclick', changeTask);
     task.innerText=newText;
     list.append(task);
     list.replaceChild(task,change);
-    list=JSON.parse(localStorage.getItem("lists"));//push the task to local storage
-    list[listId].push(JSON.stringify(newText));
-    localStorage.setItem("lists",JSON.stringify(list));
+    list=JSON.parse(localStorage.getItem("tasks"));//push the task to local storage
+    list[listId].unshift(newText);
+    localStorage.setItem("tasks",JSON.stringify(list));
 }
 //function that delete task given by text
 function deleteTaskByText(text,listName)
 {
     let check=true;
-    let listsJson=JSON.parse(localStorage.getItem("lists"));
-    let list=listsJson[listName];
+    let tasksJson=JSON.parse(localStorage.getItem("tasks"));
+    let list=tasksJson[listName];
     for (let i = 0; i < list.length; i++) {
-        if(text==JSON.parse(listsJson[listName][i])&&check)
+        if(text==tasksJson[listName][i]&&check)
         {
-            listsJson[listName].splice(i,1);
+            tasksJson[listName].splice(i,1);
             check=false;
         }
     }
-    localStorage.setItem("lists",JSON.stringify(listsJson));
+    localStorage.setItem("tasks",JSON.stringify(tasksJson));
 }
 
+
+
+//add and remove key press event
+let moveTaskLi;
 function moveTask(event)
 {
+    moveTaskLi=event.target
+    document.addEventListener("keydown", moveTaskKeyPress);
+}
+function moveTaskEnd(event)
+{
+    document.removeEventListener("keydown", moveTaskKeyPress, false);
+}
+//check if alt+number was pressed
+function moveTaskKeyPress(event)
+{
+    let list;
+    if(event.altKey&&(event.keyCode===49||event.keyCode===50||event.keyCode===51))
+    {
+        deleteTaskByText(moveTaskLi.innerText,moveTaskLi.parentElement.id)
+    }
+    if(event.keyCode===49&&event.altKey)
+    {
+        document.getElementById("todo").append(moveTaskLi);
+        list=JSON.parse(localStorage.getItem("tasks"));//push the task to local storage
+        list["todo"].unshift(moveTaskLi.innerText);
+        localStorage.setItem("tasks",JSON.stringify(list));
+    }
+    if(event.keyCode===50&&event.altKey)
+    {
+        document.getElementById("in-progress").append(moveTaskLi)
+        list=JSON.parse(localStorage.getItem("tasks"));//push the task to local storage
+        list["in-progress"].unshift(moveTaskLi.innerText);
+        localStorage.setItem("tasks",JSON.stringify(list));
+    }
+    if(event.keyCode===51&&event.altKey)
+    {        
+        document.getElementById("done").append(moveTaskLi)
+        list=JSON.parse(localStorage.getItem("tasks"));//push the task to local storage
+        list["done"].unshift(moveTaskLi.innerText);
+        localStorage.setItem("tasks",JSON.stringify(list));
+    }
+}
 
-}
-function keyW(e) {
-    if(e.keyCode === 87){
-       console.log('w');
-    };
-}
-document.onkeydown = keyW;
