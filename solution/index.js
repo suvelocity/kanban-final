cbdc7d00a657114693524360a1a37fc8fda4ec72
danielkravtsov5 <danielkravtsov5@gmail.com>
2021-09-26 11:10:47 +0300

    added event listener to submit button

---
+++ solution/index.js
@@ -0,0 +1,16 @@
+function enterButton(e)
+{
+    if (e.target.classList.contains("submit"))
+    {
+       const task = document.createElement("div")
+       const taskInput = document.getElementById(e.target.id.split("submit-")[1]+ "-task").value
+       const div = e.target.closest("section")
+       const li = document.createElement("li")
+       li.append(taskInput)
+       div.append(li)
+       localStorage.setItem(taskInput, div.id)
+       
+    }
+}
+
+document.addEventListener("click" ,  function(e){enterButton(e);});
